ef678dd835bfaa2ffb9013a15860c22e1aced193
José Brenon <eibrenon88@gmail.com>
2023-06-16 18:05:54 -0300

    Criando o primeiro código em pascal

--- pascalexercicios/media.pas
+++ pascalexercicios/media.pas
@@ -1,1 +1,11 @@
+program media;
+var
+   nota1, nota2, calc_media: real; //definição variáveis
+begin
+    nota1 := 5;  //entradas
+    nota2 := 7;
+    calc_media := (nota1 + nota2) /2; //processamento
+    writeln('A media e ',calc_media); //saída
+    readln();
 
+end.
