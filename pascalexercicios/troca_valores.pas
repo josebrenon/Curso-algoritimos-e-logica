c5a9ab7711b0859eb41d0bc35cbb61e4ab394040
José Brenon <eibrenon88@gmail.com>
2023-06-19 17:25:28 -0300

    Add exercício de troca de valores em pascal

---
+++ pascalexercicios/troca_valores.pas
@@ -0,0 +1,10 @@
+program troca_valores;
+var A, B :Integer;
+begin
+     A := 5;
+     B := 10;
+    writeln('A =', B);
+    writeln('B =', A);
+    writeln('..........FIM DO PROGRAMA.......');
+    readln();
+end.
