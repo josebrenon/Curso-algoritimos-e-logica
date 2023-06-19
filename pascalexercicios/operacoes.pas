3e7b0d84fadcedbe731ac6defa7c197bbfb04d2f
José Brenon <eibrenon88@gmail.com>
2023-06-19 16:17:04 -0300

    atualizando o código formatando os números com duas casas decimais

--- pascalexercicios/operacoes.pas
+++ pascalexercicios/operacoes.pas
@@ -1,15 +1,20 @@
 program operacoes;
 var
-   num1, num2 : real;
+   num1, num2, soma, subt, mult, divs : real;
 begin
     write('Digite o primeiro numero: ');
     readln(num1);
     write('Digite o segundo numero: ');
     readln(num2);
 
-    writeln('A soma = ',num1 + num2);
-    writeln('A subtracao = ', num1 - num2);
-    writeln('A multiplicacao = ', num1 * num2);
-    writeln('A divisao = ', num1 / num2);
+    soma := num1 + num2;
+    mult := num1 * num2;
+    subt := num1 - num2;
+    divs := num1 / num2;
+    writeln('A soma = ',soma:6:2);
+    writeln('A subtracao = ', subt:6:2);
+    writeln('A multiplicacao = ', mult:6:2);
+    writeln('A divisao = ', divs:6:2);
+    writeln('........FIM DO SISTEMA..........');
     readln();
 end.
