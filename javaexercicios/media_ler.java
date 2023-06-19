22416e7d28f5adca800aba90325e6acb6c6ed671
José Brenon <eibrenon88@gmail.com>
2023-06-19 14:29:15 -0300

    add arquivos do exercício media_ler.java

---
+++ javaexercicios/media_ler.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+class media_ler
+{
+    public static void main(String[] args)
+    {
+        float nota1, nota2, calc_media;
+
+        nota1 = Float.parseFloat(JOptionPane.showInputDialog("Digite a primeira nota: "));
+        nota2 = Float.parseFloat(JOptionPane.showInputDialog("Digite a segunda nota: "));
+        calc_media = (nota1 + nota2) /2;
+        JOptionPane.showMessageDialog(null,"Media = "+calc_media);
+        JOptionPane.showMessageDialog(null,"......FIM DO SISTEMA......");
+    }
+}
