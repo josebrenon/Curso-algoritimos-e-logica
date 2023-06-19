0fe29513b883cd6d555a8e88ad9cf0bb8583bff4
José Brenon <eibrenon88@gmail.com>
2023-06-19 16:37:27 -0300

    Add exercícios de operações em C/C++

---
+++ cc++exercicios/operacoes.cpp
@@ -0,0 +1,24 @@
+#include <conio.h>
+#include <stdio.h>
+main()
+{
+      float num1, num2, soma, subt, mult, divs;
+      
+      printf("\nDigite o primeiro numero: ");
+      scanf("%f", &num1);
+      printf("Digite o segundo numero: ");
+      scanf("%f", &num2);
+      
+      soma = num1 + num2;
+      subt = num1 - num2;
+      mult = num1 * num2;
+      divs = num1 / num2;
+      
+      printf("A soma entre %f + %f = %f", num1, num2, soma);
+      printf("\nA subtracao entre %f e %f = %f", num1, num2, subt);
+      printf("\nA multiplicacao entre %f e %f = %f", num1, num2, mult);
+      printf("\nA divisao entre %f e %f = %f", num1, num2, divs);
+      printf("\n\n\n........FIM DO SISTEMA.......");
+      getch();
+}
+
