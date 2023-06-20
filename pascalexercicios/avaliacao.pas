5c3e3cb34c2e07691e1552922240aa466449d54b
José Brenon <130095413+josebrenon@users.noreply.github.com>
2023-06-20 15:49:44 -0300

    Atualizando o cabeçalho

--- pascalexercicios/avaliacao.pas
+++ pascalexercicios/avaliacao.pas
@@ -4,7 +4,9 @@
      terceira prova tem peso 5. Faça um algoritmo para calcular a media final
      de um aluno desta disciplina.
 
-     Esse programa pede as notas das três provas e calcula sua média final.
+     Esse programa pede as notas das três provas, calcula o peso de cada uma,
+     sua média final e mostra os resultados.
+
 
      By: José Brenon - 20/06/2023
 }
