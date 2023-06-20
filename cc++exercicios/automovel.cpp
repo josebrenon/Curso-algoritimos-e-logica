73026da2ed6b27be812bb11f3f6e8ed4047b9ff8
José Brenon <eibrenon88@gmail.com>
2023-06-20 12:05:54 -0300

    Add um cabeçalho ao programa

--- cc++exercicios/automovel.cpp
+++ cc++exercicios/automovel.cpp
@@ -1,3 +1,15 @@
+/*
+    Problema: O custo ao consumidor de um carro novo, é a soma do custo de fábrica
+    com a percentagem do revendedor e com o custo dos impostos (aplicados ao custo
+    de fábrica). Supondo que a percentagem do revendedor seja e 25% do custo de fábrica
+    e que os impostos custam 45% do custo de fábrica, faça um algoritmo que leia o valor
+    e custo de fábrica e determine o preço final do automóvel (custo ao consumidor).
+     
+    Esse programa lê o custo de fábrica do carro e mostra qual a porcentagem do revendedor,
+    o quanto de imposto, e o valor final.
+       
+    By: José Brenon - 20/06/2023
+*/
 #include <conio.h>
 #include <stdio.h>
 main()
