050bdba05d59945d26a701a8ae01ea562a689015
josebrenon <130095413+josebrenon@users.noreply.github.com>
2023-07-31 14:57:14 -0300

    Atualizando os cabeçalhos do exercício saldo_se

--- cc++exercicios/saldo_se.cpp
+++ cc++exercicios/saldo_se.cpp
@@ -1,9 +1,8 @@
 /*
-    Problema: Um cliente de um banco tem um saldo positivo de R$ 500,00.
-    Fazer um algoritmo que leia um cheque que entrou e ANALISE se o cheque
-    poderá ser descontado ou não, já que este cliente não possui limite.
-    Se o cheque não poderá ser descontado, mostre essa informação,
-    caso contrário, desconte o cheque e informe o saldo na tela.
+    Problema: Fazer um algoritmo que leia o saldo inicial do cliente do banco
+    e leia um cheque que entrou e ANALISE se o cheque poderá ser descontado ou não,
+    já que este cliente não possui limite. Se o cheque não poderá ser descontado,
+    mostre essa informação, caso contrário, desconte o cheque e informe o saldo na tela.
 
     Esse programa Lê o saldo inicial, lê o valor do cheque que vai ser
     debitado, analisa se o cheque poderá ser descontado conforme o limite fixo,
