b1de0f2d3c8d2a625ef24dfdb795e1e0b3e4d2e8
José Brenon <eibrenon88@gmail.com>
2023-08-02 15:35:26 -0300

    Atualizando o cabeçalho

--- cc++exercicios/crescenteAB.cpp
+++ cc++exercicios/crescenteAB.cpp
@@ -2,7 +2,7 @@
     Problema: Crie 2 variáveis (A e B) e leia 1 valor para cada um deles.
     Disponibilize eles de forma crescente.
 
-    Esse programa pede ao usuário que digite dois valores inteiros, verifica,
+    Esse programa lê dois valores inteiros, verifica,
     se o primeiro é maior que o segundo ele mostra o segundo primeiro, se não
     mostra o primeiro antes do segundo.
 
