2e1f991b3349269cde208d702c64fe9839ba8138
José Brenon <eibrenon88@gmail.com>
2023-08-08 23:39:33 -0300

    Adicionando o exercício abc_crescente em Palcal

---
+++ pascalexercicios/abc_crescente.pas
@@ -0,0 +1,39 @@
+{
+    Problema: Ler 3 valores INTEIROS para as variáveis A, B, C.
+    Apresentar os valores dispostos em ordem crescente.
+
+    Esse programa lê três números inteiros, verifica qual deles é o maior, o do meio
+    e o menor e mostra os resultados.
+
+    Autor: José Brenon - 08/08/2023
+}
+program abc_crescente;
+var
+   a, b, c: integer;
+begin
+     write('Digite o valor de A: ');
+     readln(a);
+     write('Digite o valor de B: ');
+     readln(b);
+     write('Digite o valor de c: ');
+     readln(c);
+
+     if ((a <= b) and (a <=c)) then
+        if ((b <= c)) then
+           writeln('Em ordem crescente ficou: ', a, ' - ',b, ' - ',c)
+        else
+           writeln('Em ordem crescente ficou: ',a, ' - ', c, ' - ',b)
+     else if ((b <= a) and (b <= c)) then
+         if ((a <= c)) then
+            writeln('Em ordem crescente ficou: ',b, ' - ',a, ' - ',c)
+         else
+            writeln('Em ordem crescente ficou: ',b, ' - ',c, ' - ',a)
+     else
+         if ((a <= b)) then
+            writeln('Em ordem crescente ficou: ',c, ' - ',a, ' - ',b)
+         else
+            writeln('Em ordem crescente ficou: ',c, ' - ',b, ' - ',a);
+
+     writeln('.......FIM......');
+     readln();
+end.
