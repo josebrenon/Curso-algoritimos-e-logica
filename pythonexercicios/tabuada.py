ca9d8f0493132f771f783f61bd36fcc9d1c9ef82
José Brenon <eibrenon88@gmail.com>
2023-08-09 22:45:00 -0300

    Adicionando o exercício tabuada em Python

---
+++ pythonexercicios/tabuada.py
@@ -0,0 +1,20 @@
+"""
+    Problema: Ler uma variável de número inteiro e mostrar sua tabuada.
+
+    Esse programa lê um número inteiro e mostra a sua tabuada.
+
+    Autor: José Brenon - 09/08/2023
+"""
+n = int(input('Digite um número para ver a sua tabuada: '))
+print('='*10)
+print('{} x {:2} = {:2}'.format(n, 1, n*1))
+print('{} x {:2} = {:2}'.format(n, 2, n*2))
+print('{} x {:2} = {:2}'.format(n, 3, n*3))
+print('{} x {:2} = {:2}'.format(n, 4, n*4))
+print('{} x {:2} = {:2}'.format(n, 5, n*5))
+print('{} x {:2} = {:2}'.format(n, 6, n*6))
+print('{} x {:2} = {:2}'.format(n, 7, n*7))
+print('{} x {:2} = {:2}'.format(n, 8, n*8))
+print('{} x {:2} = {:2}'.format(n, 9, n*9))
+print('{} x {:2} = {:2}'.format(n, 10, n*10))
+print('='*10)
