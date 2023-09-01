e8eacac5e9550398e6a20e6877aa97224bf32ea2
José Brenon <eibrenon88@gmail.com>
2023-09-01 15:13:25 -0300

    Add o ex tabuada while em C/C++

---
+++ cc++exercicios/tabuada_while.cpp
@@ -0,0 +1,27 @@
+/*
+    Problema: Ler uma variável de número inteiro e mostrar sua tabuada.
+    Usando o laço de repetição Enquanto.
+
+    Esse programa lê um número inteiro e mostra a sua tabuada usando
+    o While.
+
+    Autor: José Brenon - 01/09/2023
+*/
+#include <conio.h>
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    printf("Digite um numero para saber a sua tabuada: ");
+    scanf("%d", &n);
+    i = 1;
+    while (i <= 10)
+    {
+        printf("\n%d x %d = %d", n, i, (n * i));
+        i++;
+    }
+    
+    printf("\n\n\n------FIM------");
+    return 0;
+}
+
