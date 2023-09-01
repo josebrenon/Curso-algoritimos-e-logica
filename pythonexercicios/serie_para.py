94c0170829ea11f7f4479fcfb1933b3664cb31e2
José Brenon <eibrenon88@gmail.com>
2023-09-01 16:38:53 -0300

    Add o ex serie_para em Python

---
+++ pythonexercicios/serie_para.py
@@ -0,0 +1,12 @@
+'''
+    Problema: Faça um algoritmo que gere a seguinte série:
+    10, 20, 30, 40, ... 980, 990, 1000.
+
+    Esse programa utiliza um For para gerar uma série de números
+    começando em 10 e indo até 1000 contando de 10 em 10.
+
+    Autor: José Brenon - 01/09/2023
+'''
+for c in range(10, 1001, 10):
+    print(c)
+print(f'{"FIM":.^21}')
